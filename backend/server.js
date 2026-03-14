f268d5a14e0a40e01bd078d8499f7e2db2ec1d05
Monisha0503 <monishagnanaprakasam4@gmail.com>
2026-03-14 21:59:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,25 +3,20 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 require("dotenv").config();
-
 const authRoutes = require("./routes/auth");
 const issueRoutes = require("./routes/issue");
-
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 // Static folder for images
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/issues", issueRoutes);
-
 // MongoDB Connect
-mongoose.connect(process.env.MONGO_URI)
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://monishagnanaprakasam4_db_user:6oIJxdhybS8BmvS3@cluster0.zoj9zfb.mongodb.net/civicalert?appName=Cluster0";
+mongoose.connect(MONGO_URI)
   .then(() => console.log("MongoDB Connected ✅"))
-  .catch((err) => console.log(err));
-
+  .catch((err) => console.log("MongoDB Error:", err.message));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`));
