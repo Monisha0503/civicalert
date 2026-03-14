10a0ed500158f2e1105db5e743afecac044290f9
Monisha0503 <monishagnanaprakasam4@gmail.com>
2026-03-14 21:11:41 +0530

    Fix incorrect URL in registration API call

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -14,7 +14,7 @@ function Register() {
 
   const handleSubmit = async () => {
     try {
-      await axios.post("http://https://civicalert-backend-bdgdb7h2aqbfdjgk.centralindia-01.azurewebsites.net/api/auth/register", form);
+      await axios.post("https://civicalert-backend-bdgdb7h2aqbfdjgk.centralindia-01.azurewebsites.net/api/auth/register", form);
       navigate("/");
     } catch (err) {
       setError("❌ Registration failed! Try again.");
